cc7bfdc933e39ccb27cf58ed9b858ef418ef19c7
eLoop <eloopeg@gmail.comgit config --global user.email eloop.eg@gmail.com>
2026-09-08 13:04:14 +0300

    Fix hosted documentation routing

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import app from "../src/app.js";
+import docsPage from "../src/docsPage.js";
 
 let databaseConnection;
 
@@ -29,6 +30,10 @@ export default async function handler(req, res) {
     requestUrl.startsWith("/health");
 
   try {
+    if (requestUrl === "/docs" || requestUrl === "/api/docs") {
+      res.setHeader("Content-Type", "text/html; charset=utf-8");
+      return res.status(200).send(docsPage());
+    }
     if (!isPublicHealth) await connectDatabase();
     return app(req, res);
   } catch (error) {
